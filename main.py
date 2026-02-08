d2cb8b153918c9a5703fa0707ab6f844506a75f9
Realbluple <ljcastroluna1@gmail.com>
2026-02-07 22:59:33 -0700

    Refactor input validation and add icon
    Improve user input flow and validation in main.py: add time and sys imports; enhance greet messages; make checknum return a float on success (and print an error on failure); simplify impossiblecheck logic; centralize prompting in numask and convert inputs in main, looping until valid values are provided. Calculate and print the final win rate, then sleep and exit. Also add a new winrate.ico resource. Minor whitespace and formatting cleanup included.

--- main.py
+++ main.py
@@ -1,104 +1,72 @@
 import calculations
-
+import time
+import sys
 
 VERSION = '0.0.1'
 
 
 def greet():
-
     '''greets the user, like a logo and a message and Version'''
 
     print('add logo here')
-
     print(VERSION)
-
     print("This is a calculator that will calculate your win percentage")
+    print('the program will not check for any problems till the end so if you put a letter it will repeat till the end')
 
 
 def checknum(num):
-
     '''checks if number is a str if it is turns it into a float'''
 
     try:
-
         num = float(num)
-
-        return False
-
+        return num
+    
     except ValueError:
-
      print('ERROR: please enter a number')
-
      return True
 
 
 def impossiblecheck(totalgames,wins):
-
     '''checks for impossible scenerious like if win execeeds totalgames and if wins or total games are at 0'''
 
     if wins > totalgames:
-
         print("stop trying to break the system")
-
         return True
-
-    elif wins <= 0 or totalgames <= 0:
-
+    elif totalgames <= 0:
         print('stop it!')
-
         return True
-
     else:
-
         return False
 
 def numask():
+    '''asks for a numbers then checks if numbers are good'''
 
-        '''asks for a numbers then checks if numbers are good'''
+    print("To start off, input the total amount of game played!")
+    totalgames = input("-> ")
 
-        print("To start off, input the total amount of game played!")
+    print('now enter the amount of games you won out of the total')
+    wins = input('-> ')
 
-        while True:
-
-            totalgames = input("-> ")
-
-            while checknum(totalgames):
-
-                totalgames = input("-> ")
-
-            break
-
-
-        print('now enter the amount of games you won out of the total')
-
-        while True:
-
-            wins = input('-> ')
-
-            while checknum(wins):
-
-                wins = input("-> ")
-
-            break
-
-        numberscomp = [totalgames,wins]
-
-        return numberscomp
+    numberscomp = [totalgames,wins]
+    return numberscomp
 
 
 def main():
     '''runs the main functioninality of this project'''
-    greet()
 
+    greet()
     numberscomp = numask()
+    numberscomp[0] = checknum(numberscomp[0])
+    numberscomp[1] = checknum(numberscomp[1])
 
     while impossiblecheck(numberscomp[0],numberscomp[1]):
+        numberscomp = numask()
+        numberscomp[0] = checknum(numberscomp[0])
+        numberscomp[1] = checknum(numberscomp[1])
+    finalnum = round(calculations.calculatewinper(numberscomp[0],numberscomp[1]))
+    print(f'your win rate is {finalnum}%!')
+    time.sleep(10)
+    sys.exit(0)
 
-       numberscomp =  numask()
-
-    print(numberscomp)
-
-       
 
 main()
-
